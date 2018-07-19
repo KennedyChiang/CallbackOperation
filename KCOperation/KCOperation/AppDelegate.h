4fccf4c0cce1fa198b6bbdbe3ef4116ed5e5dbc8
Kennedy Chiang <kennedychiang@ecoworkinc.com>
2018-07-19 12:02:18 +0800

    init project

---
+++ KCOperation/KCOperation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KCOperation
+//
+//  Created by Chiang ML on 2018/7/19.
+//  Copyright © 2018 Chiang ML. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KCOperation/KCOperationTests/KCOperationTests.m
@@ -0,0 +1,39 @@
+//
+//  KCOperationTests.m
+//  KCOperationTests
+//
+//  Created by Chiang ML on 2018/7/19.
+//  Copyright © 2018 Chiang ML. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KCOperationTests : XCTestCase
+
+@end
+
+@implementation KCOperationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
